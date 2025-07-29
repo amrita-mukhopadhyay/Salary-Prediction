4572350b5f63e4ab0a7555011399cf4d933dcf1d
Amrita Mukhopadhyay <123255444+amrita-mukhopadhyay@users.noreply.github.com>
2025-07-29 23:53:48 +0530

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,29 @@
+from sklearn.pipeline import Pipeline
+
+from sklearn.model selection import train test split
+
+From sklearn.wetrics import accuracy score, classification report from sklearn.linser model import
+
+Logistichagression From sklearn.ensemble import RandomforestClassifier, GradientäonstingClassifier
+
+from sklearn.neighbors inport DeighborsClassifier
+
+from sklearn, avm Import SVC
+
+from sklearn.preprocessing import StandardScaler, OnaHotEncoder
+
+Xtrain, Xtest, y train, y test train test split(x, y, test size, random_state)
+
+LI
+
+models("LogisticRegression: Logistickagression),
+
+"Bandomforest": RandomforestClassifier(),
+
+"Keighbors Classifier(),
+
+"SVM SVC(),
+
+"Gradientboosting GradientboostingClassifier()
+
+results()
---
+++ app3.py
@@ -0,0 +1,31 @@
+from sklearn.preprocessing
+
+Import LabelEncoder
+
+Bimport libarary
+
+encoder LabelEncoder()
+
+#create object
+
+data['workelwss']wencoder.fik_transform(data['workclass']) #7 categories6,1, 2, 3, 4, 5, 6,
+
+data['marital status']eencoder.fit transform(data['marital-status'])
+
+#3 categories 6, 2, 2
+
+data['occupation']eancoder.fit transform(data['occupation'])
+
+data['relationship' ]eencoder.fit transform(data['relationship'))
+
+#5 categories 8, 1, 2, 3, 4
+
+data['race']-encoder.fit transform(data['race'])
+
+data['gender']wncoder, Fit transform(data['gender'])
+
+#2 catogories.
+
+91
+
+data['native-country']-encoder.fit_transform(data['native-country'])
